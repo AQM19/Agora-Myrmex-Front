83b14a771b6704348afd3c445d6d32e65e75a55f
AQM19 <quintanalaaron@gmail.com>
2025-01-10 14:22:58 +0100

    Feat: Exportación de los componentes ui

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export * from './ui/footer/Footer';
+export * from './ui/header/Header';
+export * from './ui/logo/AQMIcon';
+export * from './ui/sidebar/Sidebar';
+export * from './ui/local-switch/LocalSwitch';
+export * from './ui/side-menu-button/SideMenuButton';
