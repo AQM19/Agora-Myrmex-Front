6db0ec1b61e85ae4a6e52e6790a2b88f8431ebc3
AQM19 <quintanalaaron@gmail.com>
2025-01-10 14:02:15 +0100

    Feat: Creación del componente del logo

---
+++ src/components/ui/logo/AQMIcon.tsx
@@ -0,0 +1,20 @@
+import { Link } from '@/i18n/routing'
+import Image from 'next/image'
+import React from 'react'
+
+const AQMIcon = () => {
+    return (
+        <Link
+            href={'/'}
+        >
+            <Image
+                src={'imgs/svg/AQM.svg'}
+                alt={'Logo de Aarón Quintanal Martín'}
+                width={40}
+                height={40}
+            />
+        </Link>
+    )
+}
+
+export default AQMIcon
