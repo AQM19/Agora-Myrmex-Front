afe75855926ab6a355a66effbf2a7b606765e6d7
AQM19 <quintanalaaron@gmail.com>
2025-01-10 14:23:05 +0100

    Feat: Creación del layour

--- src/app/[locale]/(root)/layout.tsx
+++ src/app/[locale]/(root)/layout.tsx
@@ -2,22 +2,25 @@
 
 import { useDarkModeStatus } from '@/core/services/ui/dark-mode-status.service';
 import React from 'react'
+import Header from '../../../components/ui/header/Header';
+import Sidebar from '../../../components/ui/sidebar/Sidebar';
+import Footer from '@/components/ui/footer/Footer';
 
 const MainLayout = ({ children, }: Readonly<{ children: React.ReactNode; }>) => {
 
     const isDarkModeEnabled = useDarkModeStatus(state => state.isDarkModeEnabled);
 
     return (
-        <main className={`${isDarkModeEnabled ? 'dark' : 'light'} grid grid-rows-[auto_1fr_auto] h-screen transition-all duration-300`}>
+        <main className={`${isDarkModeEnabled ? 'dark' : 'light'} grid grid-rows-[auto_auto_1fr_auto] h-screen transition-all duration-300`}>
 
-            <header className='bg-red-500 dark:bg-purple-500'>header</header>
-            <aside className='hidden'>aside</aside>
+            <Header />
+            <Sidebar />
 
             <div className='bg-blue-500'>
                 {children}
             </div>
 
-            <footer className='bg-green-500'>footer</footer>
+            <Footer />
 
         </main>
     )
