4e6a5ffd4255827c8b16bdfb18f35fef2f3c8cba
AQM19 <quintanalaaron@gmail.com>
2025-01-10 14:31:28 +0100

    Feat: Creación del video en la págia principal

--- src/app/[locale]/(root)/page.tsx
+++ src/app/[locale]/(root)/page.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const MainPage = () => {
     return (
-        <div>MainPage</div>
+        <section id='main-page'>
+            <div id='video-presentation'>
+                <video autoPlay loop muted>
+                    <source src='/videos/9656466-hd_1920_1080_24fps.mp4' type='video/mp4'/>
+                </video>
+            </div>
+        </section>
     )
 }
 
