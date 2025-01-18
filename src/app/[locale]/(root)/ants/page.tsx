b6f9bb90ba5eaca01aa86932aacc1946e3e630e6
AQM19 <quintanalaaron@gmail.com>
2025-01-18 13:51:00 +0100

    Feat: Aplicación del input para filtrar

--- src/app/[locale]/(root)/ants/page.tsx
+++ src/app/[locale]/(root)/ants/page.tsx
@@ -1,19 +1,76 @@
+'use client'
+
 import { Pagination } from '@/components';
 import AntGridCard from '@/components/cards/ant_grid_card/AntGridCard';
-import { getPaginatedAnts } from '@/core';
-import React from 'react'
+import { getPaginatedAnts, GridAnt } from '@/core';
+import { useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react'
 
-export const revalidate = 60;
+export default function AntsPage() {
+    const [ants, setAnts] = useState<GridAnt[]>([]);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [totalPages, setTotalPages] = useState<number>(0);
+    const [isLoading, setIsLoading] = useState(true);
+    const [filter, setFilter] = useState<string>('');
+    const [debouncedFilter, setDebouncedFilter] = useState('');
 
-export default async function AntsPage({ searchParams, }: { searchParams: Promise<{ [key: string]: string | string[] | undefined }> }) {
-    
-    const page = (await searchParams).page
-    const numberPage = page ? parseInt(page.toString()) : 1;
-    const { ants, currentPage, totalPages } = await getPaginatedAnts({ page: numberPage });
+    const searchParams = useSearchParams();
+    const pageString = searchParams.get('page') ?? '1';
+    const page = isNaN(Number(pageString)) ? 1 : Number(pageString);
+
+    async function fetchAnts(filter?: string) {
+        setIsLoading(true);
+        try {
+            const { ants, totalPages, currentPage } = await getPaginatedAnts({ page, filter });
+            setCurrentPage(currentPage);
+            setAnts(ants);
+            setTotalPages(totalPages);
+        } catch (error) {
+            console.error('Error fetching ants:', error);
+        } finally {
+            setIsLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        fetchAnts();
+    }, [page]);
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setDebouncedFilter(filter);
+        }, 500);
+
+        return () => clearTimeout(timer);
+    }, [filter]);
+
+    useEffect(() => {
+        if (debouncedFilter !== '') {
+            fetchAnts(debouncedFilter);
+        } else {
+            fetchAnts();
+        }
+        // Cambiar los parámetros de búsqueda (query) en la URL
+    }, [debouncedFilter, searchParams]);
+
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setFilter(event.target.value);
+    };
 
     return (
-        <>
-            <div className='grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10 justify-items-stretch p-5'>
+        <section className='p-5 flex flex-col gap-4'>
+
+            <div>
+                <span className='font-semibold text-lg'>Filtro de especies</span>
+                <input
+                    type="text"
+                    value={filter}
+                    onChange={handleInputChange}
+                    className='rounded-md w-full h-10 text-neutral-900 px-2'
+                    placeholder="Buscar por género o especie" />
+            </div>
+
+            <div className='grid grid-cols-2 sm:grid-cols-3 gap-10 mb-10 justify-items-stretch'>
                 {
                     ants.map((ant, index) => (
                         <AntGridCard
@@ -25,6 +82,6 @@ export default async function AntsPage({ searchParams, }: { searchParams: Promis
             </div>
 
             <Pagination totalPages={totalPages} />
-        </>
+        </section>
     );
 }
