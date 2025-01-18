11e1be93a6a7dbcf5481511d6acf8e3f8b9a2752
AQM19 <quintanalaaron@gmail.com>
2025-01-18 11:57:59 +0100

    Feat: Creación del card para visualizar datos

--- src/components/cards/data/DataCard.tsx
+++ src/components/cards/data/DataCard.tsx
@@ -1,23 +1,42 @@
+import { Bug } from 'lucide-react';
 import React from 'react'
 
 interface Props {
-    title: string
-    icon: string
-    value: string
+    title: string;
+    icon: string;
+    max: string | null;
+    min: string | null;
 }
 
-const DataCard = ({ title, icon, value }: Props) => {
+const DataCard = ({ title, icon, max, min }: Props) => {
     return (
-        <div className="bg-white rounded-lg shadow-lg overflow-hidden h-full">
+        <div className="bg-white rounded-lg shadow-lg overflow-hidden h-full min-h-44">
             <div className="bg-gray-800 px-3 py-2 flex items-center justify-between">
                 <span className="text-xl text-white" role="img" aria-label={title}>
                     {icon}
                 </span>
                 <h3 className="text-sm font-semibold text-white">{title}</h3>
             </div>
+
             <div className="p-3 text-center">
-                <p className="text-lg font-bold text-gray-700">{value}</p>
+                {
+                    (max !== null && min !== null) ? (
+                        <div className="flex justify-between items-end">
+                            <div className="text-center">
+                                <Bug className='w-16 h-16 mx-auto text-green-600' />
+                                <p className="mt-2 font-semibold text-gray-800">Max: {max} mm</p>
+                            </div>
+                            <div className="text-center">
+                                <Bug className='w-8 h-8 mx-auto text-amber-950' />
+                                <p className="mt-2 font-semibold text-gray-800">Min: {min} mm</p>
+                            </div>
+                        </div>
+                    ) : (
+                        <p className="text-lg font-bold text-gray-700 mt-6">No hay datos</p>
+                    )
+                }
             </div>
+
         </div>
     )
 }
