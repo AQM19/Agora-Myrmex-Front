007d540ca8f8a313939bd8115378728f837b25ca
AQM19 <quintanalaaron@gmail.com>
2025-01-18 13:50:47 +0100

    Refactor: Aplicación de filtros en el servicio

--- src/core/services/supabase/ants.service.ts
+++ src/core/services/supabase/ants.service.ts
@@ -5,28 +5,47 @@ import { Ant, GridAnt, ImageAnt, supabase } from "@/core";
 interface PaginationOptions {
     page?: number;
     take?: number;
+    filter?: string;
 }
 
-export const getPaginatedAnts = async ({ page = 1, take = 12 }: PaginationOptions) => {
+export const getPaginatedAnts = async ({ page = 1, take = 12, filter }: PaginationOptions) => {
     if (isNaN(Number(page))) page = 1;
     if (page < 1) page = 1
     if (isNaN(Number(take))) take = 12;
     if (take < 1) take = 12
 
     try {
-        const { data: ants, error } = await supabase
+        let query = supabase
             .from('ant_grid')
             .select('*')
             .order('genus', { ascending: true })
             .order('species', { ascending: true })
             .range((page - 1) * take, ((page - 1) * take) + take - 1);
 
-        const { data: dataCount, count } = await supabase
+        let queryCount = supabase
             .from('ant_grid')
             .select('*', { count: 'exact', head: true });
 
-        const totalCount: number = count || 0;
+        // Si el filtro está presente, se aplica a las columnas genus y species
+        if (filter) {
+            const filterParts = filter.split(' ');
 
+            // Si se tiene solo el género (1 palabra), se aplica al genus
+            if (filterParts[0]) {
+                query = query.ilike('genus', `%${filterParts[0]}%`);
+                queryCount = queryCount.ilike('genus', `%${filterParts[0]}%`)
+            }
+
+            // Si se tiene también la especie (2 palabras), se aplica a species
+            if (filterParts[1]) {
+                query = query.ilike('species', `%${filterParts[1]}%`);
+                queryCount.ilike('species', `%${filterParts[0]}%`)
+            }
+        }
+
+        const { data: ants, error } = await query;
+        const { data: dataCount, count } = await queryCount;
+        const totalCount: number = count || 0;
         const totalPages = Math.ceil(totalCount / take);
 
         return {
