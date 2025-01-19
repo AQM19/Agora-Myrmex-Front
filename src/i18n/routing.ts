09875c9a68831e1afa136df89d18e9768b9458d3
AQM19 <quintanalaaron@gmail.com>
2025-01-19 20:36:30 +0100

    Refactor: Aplicación de los idiomas en las rutas

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -2,14 +2,21 @@ import { defineRouting } from 'next-intl/routing';
 import { createNavigation } from 'next-intl/navigation';
 
 export const routing = defineRouting({
-    locales: ['es', 'en'],
+    locales: ['es', 'en', 'de', 'it', 'ja', 'ko', 'pl', 'pt', 'zh'],
     defaultLocale: 'es',
     localePrefix: 'always',
     pathnames: {
         "/": "/",
         "/ants/[slug]": {
-            "en": '/ants/[slug]',
-            "es": '/hormigas/[slug]'
+            'en': '/ants/[slug]',
+            'es': '/hormigas/[slug]',
+            'de': '/ameisen/[slug]',
+            'it': '/formiche/[slug]',
+            'ja': '/ari/[slug]',
+            'ko': '/gae-mi/[slug]',
+            'pl': '/mrowki/[slug]',
+            'pt': '/formigas/[slug]',
+            'zh': '/mayi/[slug]',
         }
     }
 });
