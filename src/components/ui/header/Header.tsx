93a5b377ae5d1d328def02301c89a1339a18c6af
AQM19 <quintanalaaron@gmail.com>
2025-01-19 21:55:12 +0100

    Feat: Generación de logos y mejora de la vista del header

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
 import AQMIcon from '../logo/AQMIcon'
 import SideMenuButton from '../side-menu-button/SideMenuButton';
+import Image from 'next/image';
 
 const Header = () => {
     return (
-        <header className='px-5 py-2 flex justify-between items-center bg-neutral-100 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50 transition-colors duration-300'>
+        <header className='px-5 flex justify-between items-center bg-neutral-100 text-neutral-950 dark:bg-neutral-900 dark:text-neutral-50 transition-colors duration-300'>
 
             <AQMIcon />
 
-            <h1 className='font-bold text-2xl'>
-                AGORA MYRMEX
-            </h1>
+            <Image src={'/imgs/svg/agora-myrmex-head-light-logo.png'} alt={''} width={140} height={140} className='block dark:hidden'/>
+            <Image src={'/imgs/svg/agora-myrmex-head-dark-logo.png'} alt={''} width={140} height={140} className='hidden dark:block'/>
 
             <SideMenuButton />
 
