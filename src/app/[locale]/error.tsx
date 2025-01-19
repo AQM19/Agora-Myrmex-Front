a63654ce722b4cd0755e3feb264ee2c13caaf84d
AQM19 <quintanalaaron@gmail.com>
2025-01-19 21:19:54 +0100

    Feat: Creación de las páginas para not found y error

--- src/app/[locale]/error.tsx
+++ src/app/[locale]/error.tsx
@@ -1,10 +1,31 @@
 'use client'
 
+import { Link } from '@/i18n/routing'
+import { useTranslations } from 'next-intl'
 import React from 'react'
 
 const ErrorLayoutPage = () => {
+
+    const t = useTranslations('No data');
+
     return (
-        <div>ErrorLayoutPage</div>
+        <section id='error-page' className='h-screen w-full bg-neutral-100 dark:bg-neutral-900 flex flex-col items-center justify-center'>
+
+            <article className='rounded-lg shadow-lg shadow-black dark:shadow-white px-5 py-10 text-neutral-900 dark:text-neutral-50 -mt-60 text-center'>
+
+                <h1 className='text-4xl font-bold mb-10 text-blue-600'>{t('page error')}</h1>
+
+                <p className='mb-8'>
+                    {t('error punish')}
+                </p>
+
+                <Link href={'/'} className='rounded-lg p-3 bg-neutral-900 text-neutral-50 dark:bg-neutral-100 dark:text-neutral-950'>
+                    {t('back home')}
+                </Link>
+
+            </article>
+
+        </section>
     )
 }
 
--- src/app/[locale]/not-found.tsx
+++ src/app/[locale]/not-found.tsx
@@ -1,8 +1,27 @@
+import { Link } from '@/i18n/routing'
+import { useTranslations } from 'next-intl'
 import React from 'react'
 
 const LayoutPageNotFound = () => {
+
+    const t = useTranslations('No data');
+
     return (
-        <div>LayoutPageNotFound</div>
+        <section className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <article className="p-8 bg-white rounded-lg shadow-md text-center -mt-60">
+
+                <h1 className="text-4xl font-bold text-blue-600 mb-4">{t('page not found')}</h1>
+
+                <p className="text-xl text-gray-600 mb-8">
+                    {t('not found description')}
+                </p>
+
+                <Link href={'/'} className='rounded-lg p-3 bg-neutral-900 text-neutral-50 dark:bg-neutral-100 dark:text-neutral-950'>
+                    {t('back home')}
+                </Link>
+
+            </article>
+        </section>
     )
 }
 
