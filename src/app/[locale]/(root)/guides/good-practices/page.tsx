c2b3af73ca85efbe56e43e835213326252213ff3
AQM19 <quintanalaaron@gmail.com>
2025-01-20 22:58:55 +0100

    Generación de páginas base de la aplicación

---
+++ src/app/[locale]/(root)/guides/good-practices/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const GoodPracticesPage = () => {
+    return (
+        <div>GoodPracticesPage</div>
+    )
+}
+
+export default GoodPracticesPage
---
+++ src/app/[locale]/(root)/guides/identification/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const IdentificationPage = () => {
+    return (
+        <div>IdentificationPage</div>
+    )
+}
+
+export default IdentificationPage
