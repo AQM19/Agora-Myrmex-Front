15d0a72d88133ad51141b688a508557e45a2a507
AQM19 <quintanalaaron@gmail.com>
2025-01-25 13:28:05 +0100

    Fix: Ajuste de los colores en about

--- src/app/[locale]/(root)/about/page.tsx
+++ src/app/[locale]/(root)/about/page.tsx
@@ -8,9 +8,9 @@ const AboutPage = () => {
     const t = useTranslations('About');
 
     return (
-        <section id='about-me' className='container mx-auto px-4 py-8 text-justify'>
+        <section id='about-me' className='container mx-auto px-4 py-8 text-justify text-neutral-900 dark:text-neutral-50'>
 
-            <div className="bg-gray-100 p-6 rounded-lg shadow-lg shadow-black mb-8">
+            <div className="bg-gray-100 dark:bg-neutral-700 p-6 rounded-lg shadow-lg shadow-black mb-8">
                 <h1 className="text-4xl font-bold mb-6 text-center">{t('title')}</h1>
 
                 <div className='flex flex-col'>
@@ -43,7 +43,7 @@ const AboutPage = () => {
             </div>
 
             <div className='grid grid-cols-1 md:grid-cols-[auto_1fr] gap-8 mb-8'>
-                <div className='bg-gray-100 p-6 rounded-lg shadow-lg shadow-black'>
+                <div className='bg-gray-100 dark:bg-neutral-700 p-6 rounded-lg shadow-lg shadow-black'>
                     <h2 className="text-2xl font-semibold mb-4">{t('highlights')}</h2>
                     <ul className="list-disc pl-6 mb-6">
                         <li className="mb-2">{t('h-1')}</li>
@@ -54,7 +54,7 @@ const AboutPage = () => {
                     </ul>
                 </div>
 
-                <div className='bg-gray-100 p-6 rounded-lg shadow-lg shadow-black'>
+                <div className='bg-gray-100 dark:bg-neutral-700 p-6 rounded-lg shadow-lg shadow-black'>
                     <h2 className="text-2xl font-semibold mb-4">{t('mission')}</h2>
                     <p className="text-lg mb-6">
                         {t('m-1')}
@@ -65,7 +65,7 @@ const AboutPage = () => {
 
             <div className='grid grid-cols-1 md:grid-cols-[1fr_auto] gap-8 mb-8'>
 
-                <div className="bg-gray-100 p-6 rounded-lg shadow-lg shadow-black">
+                <div className="bg-gray-100 dark:bg-neutral-700 p-6 rounded-lg shadow-lg shadow-black">
                     <h2 className="text-2xl font-semibold mb-4">{t('get involved')}</h2>
                     <p className="text-lg mb-4">
                         {t('i-1')}
@@ -78,7 +78,7 @@ const AboutPage = () => {
                     </Link>
                 </div>
 
-                <div className='bg-gray-100 p-6 rounded-lg shadow-lg shadow-black'>
+                <div className='bg-gray-100 dark:bg-neutral-700 p-6 rounded-lg shadow-lg shadow-black'>
                     <h2 className="text-2xl font-semibold mb-4">{t('future features')}</h2>
                     <ul className="list-disc pl-6 mb-6">
                         <li className="mb-2">{t('f-1')}</li>
